133ed0ab03864d7666e0692a3bf6062966909b7c
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2020-02-04 16:46:28 +0000

    bug fix.

--- app.rb
+++ app.rb
@@ -13,15 +13,15 @@ require_relative "controllers/machines_controller.rb"
 
 require_relative "router"
 
-employees_csv = "./data/employees.csv"
+employees_csv = File.join(__dir__,"data/employees.csv")
 employee_repository = EmployeeRepository.new(employees_csv)
 sessions_controller = SessionsController.new(employee_repository)
 
-transactionlog_csv = "./data/transactionlog.csv"
+transactionlog_csv = File.join(__dir__,"data/transactionlog.csv")
 transactionlog_repository = TransactionlogRepository.new(transactionlog_csv)
 transactionlogs_controller = TransactionlogsController.new(transactionlog_repository, sessions_controller)
 
-items_csv = "./data/items.csv"
+items_csv = File.join(__dir__,"data/items.csv")
 item_repository = ItemRepository.new(items_csv)
 machines_controller = MachinesController.new(item_repository, transactionlog_repository, sessions_controller)
 
